a37bb9d9d723fd98c06446ece4f3979c44b5998c
Lirys <iamyourmarkieboo@gmail.com>
2024-07-25 15:03:42 +0800

    Initial Commit

--- src/components/articles/Projects.vue
+++ src/components/articles/Projects.vue
@@ -1,5 +1,5 @@
 <script setup>
-import {octo} from '../../octo/persist_octo.js.mjs'
+import {octo} from '../../../.github/action-scripts/persist_octo.js.mjs'
 function load() {
   return octo().request('GET /users/{username}/repos', {
     username: "aVerySmallSoap",
