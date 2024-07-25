87a83b9e799deec192bba24e10179ef052731edf
Lirys <iamyourmarkieboo@gmail.com>
2024-07-25 20:00:32 +0800

    Change config

--- vite.config.js
+++ vite.config.js
@@ -3,6 +3,14 @@ import vue from '@vitejs/plugin-vue'
 
 // https://vitejs.dev/config/
 export default defineConfig({
+  rollupOptions:{
+    external: ['vue'],
+    output: {
+      globals: {
+        vue: 'Vue',
+      }
+    }
+  },
   base: '/portfolio/',
   plugins: [vue()],
 })
